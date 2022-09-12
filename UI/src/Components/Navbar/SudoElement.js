42e5b350815a7911124a273da1038f93babc86c7
utsav gautam <g.utsav34@gmail.com>
2022-09-12 18:11:35 +0530

    fixed banner

--- UI/src/Components/Navbar/SudoElement.js
+++ UI/src/Components/Navbar/SudoElement.js
@@ -1,10 +1,10 @@
 import React from "react";
 import './Navbar.css'
 
-function SudoElement(){
-    return(
+function SudoElement() {
+    return (
         <div className="sudo-element">
-            <img src="./Images/mlImage1.jfif"/>
+            <img src="./Images/mlImage.jpg" />
             <div className='text-holder'>
                 <h2 className="items">Machine Leaning</h2>
                 <h2 className="items">Cloud Computing</h2>
